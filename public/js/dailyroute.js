70f0ed3ec59993581e8d7fb141e2ca28ae29eeca
Nitsan Levin <Nitsan Levin>
2017-06-06 19:21:55 +0300

    changes

--- public/js/dailyroute.js
+++ public/js/dailyroute.js
@@ -26,7 +26,7 @@ dailyRoute.controller('dailyController', ['$scope', '$http', '$compile', functio
         chosenRoute = localStorage.getItem("chosenRoute");
         //if there is a chosen route for the day 
         console.log(chosenRoute);
-        if(chosenRoute != "null"){
+        if(chosenRoute != "null" || chosenRoute != null){
             console.log("there is a chosen route planned for today");
             chosenRoute = JSON.parse(localStorage.getItem("chosenRoute")); //getting the current route
             //htmlContent ="";
