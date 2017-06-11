68adfe33a90279222c0d8ada6ff033e86f97b08f
Nitsan Levin <Nitsan Levin>
2017-06-11 14:16:29 +0300

    changes

--- public/js/routeform.js
+++ public/js/routeform.js
@@ -31,20 +31,22 @@ routeForm.controller('FormController', ['$scope', '$rootScope', '$http', '$parse
         $scope.diffArr = [{'name': 'ללא העדפה', 'value': 'ללא'}, {'name': 'קל', 'value': 'קל'}, {'name': 'בינוני', 'value': 'בינוני'}, {'name': 'קשה', 'value': 'קשה'}];
         $scope.typeArr = [{'name': 'ללא העדפה', 'value': 'ללא'}, {'name': 'מתאים למשפחות', 'value': 'מתאים למשפחות'}, {'name': 'מתאים לבעלי מוגבלויות', 'value': 'מתאים לבעלי מוגבלויות'}, {'name': 'מאתגר', 'value': 'מאתגר'}, {'name': 'מיטיבי לכת', 'value': 'מיטיבי לכת'}];
 
+        var tmpPoints;
         for(var i=0; i<$scope.startPts.length; i++){
             var northStringName = 'northPoints'+startPts[i].area_id;
             var openModel = $parse(northStringName);
             openModel.assign($scope, startPts[i].points);
-            /*var southStringName = 'southPoints'+startPts[i].area_id;
+            var tmpPoints = startPts[i].points;
+            var southStringName = 'southPoints'+startPts[i].area_id;
             var openModel1 = $parse(southStringName);
-            openModel1.assign($scope, startPts[i].points.reverse());*/
+            openModel1.assign($scope, tmpPoints.reverse());
         }
 
-        for(var i=0; i<$scope.startPts.length; i++){
+        /*for(var i=0; i<$scope.startPts.length; i++){
             var southStringName = 'southPoints'+startPts[i].area_id;
             var openModel1 = $parse(southStringName);
             openModel1.assign($scope, startPts[i].points.reverse());
-        }
+        }*/
 
         //if the user had planned a route and go back to it
         if(localStorage.getItem("suggestedBack") == "true"){
