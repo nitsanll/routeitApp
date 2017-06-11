3ca26e1bbe560b8b714f8f7c51fa45e2e3f285ef
Nitsan Levin <Nitsan Levin>
2017-06-11 13:46:15 +0300

    changes

--- public/js/routeform.js
+++ public/js/routeform.js
@@ -114,7 +114,7 @@ routeForm.controller('FormController', ['$scope', '$rootScope', '$http',function
             $scope.sPoint = $scope.startPts[area.area_id].points[0];     
         }
         else {
-            console.log($scope.dir);
+            console.log($scope.dir.value);
             $scope.northPointsArr = $scope.startPts[area.area_id].points;
             $scope.southPointsArr = $scope.startPts[area.area_id].points.reverse();
             //if(area.area == "הגליל העליון") $scope.southPointsArr.splice((($scope.southPointsArr.length)-1),1);
