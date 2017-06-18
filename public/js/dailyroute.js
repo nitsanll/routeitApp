e7bf33eed98f3946ac974cd33f8af1e5d25556f4
Nitsan Levin <Nitsan Levin>
2017-06-18 19:29:27 +0300

    changes

--- public/js/dailyroute.js
+++ public/js/dailyroute.js
@@ -74,7 +74,7 @@ dailyRoute.controller('dailyController', ['$scope', '$http', '$compile', functio
                         }
                     }
                     //htmlContent+='<p id="dailyDiff" class="dailyTripDetail"> <b> רמת קושי: </b><br>' + diff + '</p>'
-                    htmlContent+='<p class="dailyTripDetail" id="dailyDuration"><p><b> משך המסלול: </b><br>' + duration + ' שעות</p><p id="dailyDiff"> <b> רמת קושי: </b><br>' + diff + '</p></p>'//<button class="endTrip" ng-click="endTrip()"> הפסק טיול </button></p>'
+                    htmlContent+='<p class="dailyTripDetail" id="dailyDuration"><span><b> משך המסלול: </b><br>' + duration + ' שעות</span><span id="dailyDiff"> <b> רמת קושי: </b><br>' + diff + '</span></p>'//<button class="endTrip" ng-click="endTrip()"> הפסק טיול </button></p>'
                     +'<p class="dailyTripDetail" id="dailyAlert" ng-click="showAlert()"><img src="images/ADD_ALERT.png"><br><span id="appendAlert"> הוסף התראה </span></p><div class="clear"></div></div>';
                     console.log(htmlContent);
                     var linkingFunction = $compile(htmlContent);
